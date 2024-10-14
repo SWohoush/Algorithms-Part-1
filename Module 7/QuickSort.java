311e2609f733736bf3f73bf728c997595bc80427
SWohoush <148148647+SWohoush@users.noreply.github.com>
2024-10-14 21:23:27 +0300

    Add files via upload

---
+++ Module 7/QuickSort.java
@@ -0,0 +1,29 @@
+public class QuickSort {
+
+    public static int partition(Comparable[] a, int lo, int hi) {
+        int i = lo;
+        int j = hi;
+        while (true) {
+            while (i <= j && a[lo].compareTo(a[i]) > 0) i++;
+            while (j > lo && a[lo].compareTo(a[j]) < 0) j--;
+            if (i >= j) break;
+            swap(a, i, j);
+        }
+        swap(a, j, lo);
+        return j;
+    }
+
+    public static void swap(Comparable[] a, int i, int j) {
+        Comparable temp = a[i];
+        a[i] = a[j];
+        a[j] = temp;
+    }
+
+    public static void sort(Comparable[] a, int lo, int hi) {
+        if (lo >= hi) return;
+        int j = partition(a, lo, hi);
+        sort(a, lo, j - 1);
+        sort(a, j + 1, hi);
+    }
+
+}
